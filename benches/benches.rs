b5c06f120a9f46513d7073a78dc796045071ac9c
Coda Hale <coda.hale@gmail.com>
2018-06-14 17:44:24 -0700

    Modernize benchmarks.

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,26 +1,28 @@
-#![feature(collections, test)]
-
+#[macro_use]
+extern crate criterion;
 extern crate rand;
 extern crate sss;
-extern crate test;
 
+use criterion::Criterion;
 use std::collections::HashMap;
-use test::Bencher;
 
-#[bench]
-fn bench_combine(b: &mut Bencher) {
+fn combine(c: &mut Criterion) {
     let mut expected: HashMap<u8, Vec<u8>> = HashMap::new();
     expected.insert(1, vec![118, 163, 66, 80, 187]);
     expected.insert(2, vec![239, 91, 129, 172, 98]);
     expected.insert(3, vec![152, 250, 192, 248, 220]);
 
-    b.iter(|| sss::combine(&expected))
+    c.bench_function("combine", move |b| b.iter(|| sss::combine(&expected)));
 }
 
-#[bench]
-fn bench_split(b: &mut Bencher) {
+fn split(c: &mut Criterion) {
     let mut rng = rand::ChaChaRng::new_unseeded();
     let input = vec![1, 2, 3, 4, 5];
 
-    b.iter(|| sss::split(5, 3, &input, &mut rng))
+    c.bench_function("split", move |b| {
+        b.iter(|| sss::split(5, 3, &input, &mut rng))
+    });
 }
+
+criterion_group!(benches, combine, split);
+criterion_main!(benches);
