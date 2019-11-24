f5d40a95295afec2355daf7ab4812f4e906cad11
Coda Hale <coda.hale@gmail.com>
2019-11-24 12:08:37 -0700

    Fix benchmarks.

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,9 +1,13 @@
 extern crate criterion;
+extern crate rand;
+extern crate rand_chacha;
 extern crate sss;
 
 use std::collections::HashMap;
 
 use criterion::{criterion_group, criterion_main, Criterion};
+use rand::SeedableRng;
+use rand_chacha::ChaChaRng;
 
 fn combine(c: &mut Criterion) {
     let mut expected: HashMap<u8, Vec<u8>> = HashMap::new();
@@ -16,8 +20,11 @@ fn combine(c: &mut Criterion) {
 
 fn split(c: &mut Criterion) {
     let input = vec![1, 2, 3, 4, 5];
+    let mut rng = ChaChaRng::from_seed([0u8; 32]);
 
-    c.bench_function("split", move |b| b.iter(|| sss::split(5, 3, &input)));
+    c.bench_function("split", move |b| {
+        b.iter(|| sss::split(5, 3, &input, &mut rng))
+    });
 }
 
 criterion_group!(benches, combine, split);
