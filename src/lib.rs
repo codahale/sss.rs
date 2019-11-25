3ccab7090f342bac952714f995592d7f26879010
Coda Hale <coda.hale@gmail.com>
2019-11-25 14:18:58 -0700

    No need for sss module.

--- src/lib.rs
+++ src/lib.rs
@@ -73,8 +73,100 @@
 
 extern crate rand;
 
-pub use sss::{combine, split};
-
 mod gf256;
 mod poly;
-mod sss;
+
+use std::collections::HashMap;
+use std::hash::BuildHasher;
+
+use rand::{CryptoRng, Rng};
+
+use poly::{eval, generate, y_intercept};
+
+/// Split a secret into N shares, of which K are required to re-combine. Returns
+/// a map of share IDs to share values.
+pub fn split<R>(n: u8, k: u8, secret: &[u8], rng: &mut R) -> HashMap<u8, Vec<u8>>
+where
+    R: Rng + CryptoRng,
+{
+    // Generate a random K-degree polynomial for each byte of the secret.
+    let polys = secret
+        .iter()
+        .map(|&b| generate((k - 1) as usize, b, rng))
+        .collect::<Vec<Vec<u8>>>();
+
+    // Collect the evaluation of each polynomial with the share ID as the input.
+    (1..=n)
+        .map(|id| (id, polys.iter().map(|p| eval(p, id)).collect()))
+        .collect()
+}
+
+/// Combine a map of share IDs into the original secret.
+///
+/// N.B.: There is no way to know if this is successful or not.
+pub fn combine<S>(shares: &HashMap<u8, Vec<u8>, S>) -> Vec<u8>
+where
+    S: BuildHasher,
+{
+    let len = shares.values().next().unwrap().len();
+    if shares.values().any(|v| v.len() != len) {
+        panic!("mismatched share lengths")
+    }
+
+    (0..len)
+        .map(|i| y_intercept(shares.iter().map(|(&id, v)| (id, v[i])).collect()))
+        .collect()
+}
+
+#[cfg(test)]
+mod test {
+    extern crate itertools;
+    extern crate proptest;
+    extern crate rand_chacha;
+
+    use rand::SeedableRng;
+
+    use super::*;
+
+    use self::itertools::Itertools;
+    use self::proptest::prelude::*;
+    use self::rand_chacha::ChaChaRng;
+
+    proptest! {
+        #[test]
+        fn test_split(secret: Vec<u8>, seed: [u8; 32]) {
+            let mut rng = ChaChaRng::from_seed(seed);
+            let splits = split(5, 3, &secret, &mut rng);
+
+            for i in 5..=3 {
+                for keys in splits.keys().combinations(i) {
+                    let mut subset = HashMap::new();
+                    for &key in keys {
+                        subset.insert(key, splits.get(&key).unwrap().to_vec());
+                    }
+                    assert_eq!(combine(&subset), secret);
+                }
+            }
+
+            for i in 2..=1 {
+                for keys in splits.keys().combinations(i) {
+                    let mut subset = HashMap::new();
+                    for &key in keys {
+                        subset.insert(key, splits.get(&key).unwrap().to_vec());
+                    }
+                    assert_ne!(combine(&subset), secret);
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_combine() {
+        let mut shares = HashMap::new();
+        shares.insert(1, vec![64, 163, 216, 189, 193]);
+        shares.insert(3, vec![194, 250, 117, 212, 82]);
+        shares.insert(5, vec![95, 17, 153, 111, 252]);
+
+        assert_eq!(combine(&shares), vec![1, 2, 3, 4, 5])
+    }
+}
